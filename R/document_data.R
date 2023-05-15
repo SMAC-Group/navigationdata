a7bb16c8662736e9367310b0ba6fc8079f1a0429
Lionel Voirol <lionelvoirol@hotmail.com>
2023-05-15 10:08:04 +0200

    initial commit, transfered data file

---
+++ R/document_data.R
@@ -0,0 +1,96 @@
+#' Example trajectory 2 in ellipsoidal coordinates
+#'
+#'
+#' @description Example trajectory 2 in ellipsoidal coordinates.
+#'
+#'
+"example_2_traj_ellipsoidal"
+#'
+#'
+#'
+
+
+
+#' Example trajectory 2 in NED coordinates
+#'
+#'
+#' @description Example trajectory 2 in NED coordinates.
+#'
+#'
+"example_2_traj_ned"
+#'
+#'
+
+
+
+#' Example trajectory 3 in ellipsoidal coordinates
+#'
+#'
+#' @description Example trajectory 3 in ellipsoidal coordinates.
+#'
+#'
+"example_3_traj_ellipsoidal"
+#'
+
+#' Example trajectory 3 in NED coordinates
+#'
+#'
+#' @description Example trajectory 3 in NED coordinates.
+#'
+#'
+"example_3_traj_ned"
+#'
+
+
+
+
+#' Example trajectory 4 in ellipsoidal coordinates
+#'
+#'
+#' @description Example trajectory 4 in ellipsoidal coordinates.
+#'
+#'
+"example_4_traj_ellipsoidal"
+#'
+
+
+
+
+#' Example trajectory 4 in NED coordinates
+#'
+#'
+#' @description Example trajectory 4 in NED coordinates.
+#'
+#'
+"example_4_traj_ned"
+#'
+
+
+#' Example trajectory 5 in ellipsoidal coordinates
+#'
+#'
+#' @description Example trajectory 5 in ellipsoidal coordinates.
+#'
+#'
+"example_5_traj_ellipsoidal"
+#'
+#'
+
+
+
+
+
+
+
+
+
+#' Example trajectory 5 in NED coordinates
+#'
+#'
+#' @description Example trajectory 5 in NED coordinates.
+#'
+#'
+"example_5_traj_ned"
+#'
+#'
+
